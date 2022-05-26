244882cad88e268c0542dd686f2c0bba821f2590
pragnakasthruri <kpragna25@gmail.com>
2022-05-26 14:38:03 +0530

    Adding the code for using lombok for getters and setters

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbook.dto;
 
-public class ContactDTO {
+import lombok.Data;
+
+public @Data class ContactDTO {
 
     public String firstName;
     public String lastName;
@@ -9,29 +11,5 @@ public class ContactDTO {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactDTO{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
     }
 }
--- src/main/java/com/bridgelabz/addressbook/model/Contact.java
+++ src/main/java/com/bridgelabz/addressbook/model/Contact.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.addressbook.model;
 
 import com.bridgelabz.addressbook.dto.ContactDTO;
+import lombok.Data;
 
+@Data
 public class Contact {
     
     private int contactId;
@@ -13,29 +15,5 @@ public class Contact {
         this.contactId =  contactId;
         this.firstName =  contactDTO.firstName;
         this.lastName = contactDTO.lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
     }
 }
