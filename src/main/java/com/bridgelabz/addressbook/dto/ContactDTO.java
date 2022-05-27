faa0ba9480cf3951cf4a8164b4cc921d2fb53e2b
pragnakasthruri <kpragna25@gmail.com>
2022-05-27 10:43:48 +0530

    Adding the code to throw custom exception if validation fails

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -16,27 +16,27 @@ public @Data class ContactDTO {
     public String lastName;
 
     @Pattern(regexp = "^[0-9]{2}?[\\s,-]{0,1}[6-9]{1}[0-9]{9}$", message = "Phone Number is Invalid ")
-    @NotEmpty(message = "last name cannot be null")
+    @NotEmpty(message = "Phone Number cannot be null")
     public String phoneNumber;
 
     @Pattern(regexp = "^[#.0-9a-zA-Z\\s,-]+$", message = "Address is Invalid")
     @NotEmpty(message = "Address cannot be null")
     public String address;
 
-    @Pattern(regexp = "^[A-Za-z]{3,}$")
+    @Pattern(regexp = "^[A-Za-z]{3,}$", message = "City name is Invalid")
     @NotEmpty(message = "City cannot be null")
     public String city;
 
-    @Pattern(regexp = "^[A-Za-z]{2,}$")
+    @Pattern(regexp = "^[A-Za-z]{2,}$", message = "State Name is Invalid")
     @NotEmpty(message = "State cannot be null")
     public String state;
 
-    @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$")
-    @NotEmpty(message = "zipcode cannot be null")
+    @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$", message = "Zipcode is Invalid")
+    @NotEmpty(message = "Zipcode cannot be null")
     public String zipcode;
 
-    @Pattern(regexp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")
-    @NotEmpty(message = "email cannot be null")
+    @Pattern(regexp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$", message = "Email is Invalid")
+    @NotEmpty(message = "Email cannot be null")
     public String email;
 
     public ContactDTO(String firstName, String lastName, String phoneNumber, String address, String city,
