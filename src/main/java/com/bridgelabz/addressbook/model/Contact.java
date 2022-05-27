d4314a167cdd5dfa8364c0808b3e8b964aab19f7
pragnakasthruri <kpragna25@gmail.com>
2022-05-27 14:37:01 +0530

    Adding the code for saving the data to mysql db

--- src/main/java/com/bridgelabz/addressbook/model/Contact.java
+++ src/main/java/com/bridgelabz/addressbook/model/Contact.java
@@ -3,18 +3,34 @@ package com.bridgelabz.addressbook.model;
 import com.bridgelabz.addressbook.dto.ContactDTO;
 import lombok.Data;
 
-@Data
-public class Contact {
-    
+import javax.persistence.*;
+
+@Entity
+@Table(name="addressbook_db")
+public @Data class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "contact_id")
     private int contactId;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
-    private String phoneNumber;
+    @Column(name = "address")
     private String address;
-    private String city;
+    @Column(name = "state")
     private String state;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "zip")
     private String zipcode;
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+    @Column(name = "email")
     private String email;
+
+    public Contact() {
+    }
 
     public Contact(int contactId, ContactDTO contactDTO) {
         super();
--- src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
@@ -3,6 +3,8 @@ package com.bridgelabz.addressbook.service;
 import com.bridgelabz.addressbook.dto.ContactDTO;
 import com.bridgelabz.addressbook.exception.AddressBookException;
 import com.bridgelabz.addressbook.model.Contact;
+import com.bridgelabz.addressbook.repository.AddressBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,6 +12,10 @@ import java.util.List;
 
 @Service
 public class AddressBookService implements IAddressBookService{
+
+    @Autowired
+    private AddressBookRepository addressBookRepository;
+
 
     List<Contact> contactList = new ArrayList<>();
 
@@ -26,9 +32,9 @@ public class AddressBookService implements IAddressBookService{
 
     @Override
     public Contact createContact(ContactDTO contactDTO) {
-        List<Contact> contactData = this.getContact();
-        contactList.add( new Contact(contactData.size() + 1, contactDTO));
-        return contactList.get(contactList.size() - 1);
+        Contact contactData = new Contact(contactList.size()+1, contactDTO);
+        contactList.add(contactData);
+        return addressBookRepository.save(contactData);
     }
 
     @Override
